135372690808c68f9a30d2428bbd9b15536de060
wyb <wyb8813@gamil.com>
2024-02-01 09:55:26 +0800

    增加了两个参数,

--- config/default.py
+++ config/default.py
@@ -3,8 +3,8 @@ cfg = Node()
 cfg.seed = 324
 cfg.dataset = 'forest_cover_type'
 cfg.task = 'classification'
-cfg.resume_dir = ''
-cfg.logname = ''
+cfg.resume_dir = 'test_resume'
+cfg.logname = 'test.log'
 
 cfg.model = Node()
 cfg.model.base_outdim = 64
@@ -18,3 +18,7 @@ cfg.fit.max_epochs = 4000
 cfg.fit.patience = 1500
 cfg.fit.batch_size = 8192
 cfg.fit.virtual_batch_size = 256
+
+
+cfg.fit.weight_decay = 1e-5
+cfg.fit.schedule_step = 20
